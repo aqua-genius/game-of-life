50609d20a737dbe557eec2c28135e3e42ce6499c
airt <airtial@gmail.com>
2017-06-07 17:10:15 +0800

    update exec

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,42 @@
 module Main where
 
-import Lib
+import Options.Applicative
+import Data.Semigroup ((<>))
 
 main :: IO ()
-main = putStrLn "it works"
+main = initialize =<< execParser options
+
+initialize :: Args -> IO ()
+initialize (Args seedFile interval singleRun) = do
+  putStrLn $ "seed-file-name = " ++ show seedFile
+  putStrLn $ "interval = " ++ show interval
+  putStrLn $ "singleRun = " ++ show singleRun
+
+data Args = Args {
+  seedFile :: String,
+  interval :: Int,
+  singleRun :: Bool
+}
+
+parser :: Parser Args
+parser = Args <$>
+  argument str (
+    metavar "FILE" <>
+    help "The seed"
+  ) <*>
+  option auto (
+    short 'i' <>
+    long "interval" <>
+    metavar "INT" <>
+    help "Interval of iterations" <>
+    showDefault <>
+    value 20
+  ) <*>
+  switch (
+    long "single-run"
+  )
+
+options :: ParserInfo Args
+options = info (parser <**> helper) $
+  fullDesc <>
+  header "Conway's Game of Life"
